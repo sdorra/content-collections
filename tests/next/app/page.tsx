be8d96ff9d660b5a73d1b06befc77b7188084825
Sebastian Sdorra <s.sdorra@gmail.com>
2024-01-03 21:34:14 +0100

    Fix hydration issue

--- tests/next/app/page.tsx
+++ tests/next/app/page.tsx
@@ -4,22 +4,20 @@ export default function Home() {
     <main className="space-y-5 p-10">
       <h1 className="text-4xl font-bold">Collections</h1>
       <div>
-      <h2 className="text-xl font-semibold mb-2">Posts</h2>
-      <ul>
-        {allPosts.map((post) => (
-          <li key={post._meta.path}>{post.title}</li>
-        ))}
-      </ul>
+        <h2 className="text-xl font-semibold mb-2">Posts</h2>
+        <ul>
+          {allPosts.map((post) => (
+            <li key={post._meta.path}>{post.title}</li>
+          ))}
+        </ul>
       </div>
       <div>
-      <h2 className="text-xl font-semibold mb-2">Authors</h2>
-      <ul>
-        <li>
+        <h2 className="text-xl font-semibold mb-2">Authors</h2>
+        <ul>
           {allAuthors.map((author) => (
             <li key={author._meta.path}>{author.displayName}</li>
           ))}
-        </li>
-      </ul>
+        </ul>
       </div>
     </main>
   );
