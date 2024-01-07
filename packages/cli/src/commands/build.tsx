b0ed8f9e883d7fc56a5a41ddfd5548a2bedae0ce
Sebastian Sdorra <s.sdorra@gmail.com>
2024-01-07 14:19:06 +0100

    Use events for logging from cli

--- packages/cli/src/commands/build.tsx
+++ packages/cli/src/commands/build.tsx
@@ -1,6 +1,8 @@
 import { createBuilder } from "@content-collections/core";
 
 export default async function build(config: string) {
+  // TODO: add error handling
+
   console.log("Building collections...");
   console.time("Finished building collections");
   const builder = await createBuilder(config);
--- packages/cli/src/commands/watch.tsx
+++ packages/cli/src/commands/watch.tsx
@@ -1,11 +1,30 @@
 import { createBuilder } from "@content-collections/core";
+import path from "path";
 
 export default async function watch(configPath: string) {
-  console.log("start initial build");
-  console.time("finished initial build");
   const builder = await createBuilder(configPath);
+
+  // TODO: log errors
+
+  builder.on("build:start", () => {
+    console.log("build started ...");
+  });
+  builder.on("build:end", (event) => {
+    console.log(
+      "... finished build in",
+      event.endedAt - event.startedAt + "ms"
+    );
+  });
+  builder.on("watch:file-changed", (event) => {
+    const relativePath = path.relative(process.cwd(), event.filePath);
+    if (event.modification === "delete") {
+      console.log("... file deleted", relativePath);
+    } else {
+      console.log("... file changed", relativePath);
+    }
+  });
+
   await builder.build();
-  console.timeEnd("finished initial build");
 
   console.log("start watching ...");
   builder.watch();
