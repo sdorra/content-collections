630f0c140becfeddff4704d191028ec703eb3b5d
Sebastian Sdorra <s.sdorra@gmail.com>
2024-01-19 13:16:46 +0100

    sample(vite): use @vitejs/plugin-react instead @vitejs/plugin-react-swc
    Use normal react plugin because swc is not supported in stackblitz

--- samples/mdx/vite.config.ts
+++ samples/mdx/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import contentCollections from "@content-collections/vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), contentCollections()],
