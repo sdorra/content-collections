e06bd359e2450f834ddddace19bb25dae77918da
Sebastian Sdorra <s.sdorra@gmail.com>
2024-04-14 21:15:50 +0200

    chore(website): add github link to root layout

--- website/app/components/Header.tsx
+++ website/app/components/Header.tsx
@@ -1,19 +1,52 @@
 import Link from "next/link";
 
-type NavLinkProps = {
+type ExternalLinkProps = {
   href: string;
+  title?: string;
+  className?: string;
   children: React.ReactNode;
 };
 
-function NavLink({ href, children }: NavLinkProps) {
+function ExternalLink({ href, title, className, children }: ExternalLinkProps) {
+  return (
+    <a
+      href={href}
+      title={title}
+      className={className}
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      {children}
+    </a>
+  );
+}
+
+type LinkCmp = typeof ExternalLink | typeof Link;
+
+function createLinkCmp(href: string): LinkCmp {
+  if (href.includes("://")) {
+    return ExternalLink;
+  }
+  return Link;
+}
+
+type NavLinkProps = {
+  href: string;
+  title?: string;
+  children: React.ReactNode;
+};
+
+function NavLink({ href, title, children }: NavLinkProps) {
+  const LinkCmp = createLinkCmp(href);
   return (
     <li>
-      <Link
+      <LinkCmp
         href={href}
+        title={title}
         className="relative hover:text-white after:h-0.5 after:absolute after:bottom-0 after:bg-primary-600 after:left-0 after:w-0 after:transition-all after:hover:w-full"
       >
         {children}
-      </Link>
+      </LinkCmp>
     </li>
   );
 }
@@ -24,7 +57,15 @@ export function Header() {
       <nav className="px-4 py-2 max-w-5xl mx-auto">
         <ul className="flex gap-4 justify-end font-semibold">
           <NavLink href="/">Home</NavLink>
-          <NavLink href="/docs">Docs</NavLink>
+          <NavLink href="/docs" title="Documentation">
+            Docs
+          </NavLink>
+          <NavLink
+            href="https://github.com/sdorra/content-collections"
+            title="GitHub repository of Content Collections"
+          >
+            GitHub
+          </NavLink>
         </ul>
       </nav>
     </header>
