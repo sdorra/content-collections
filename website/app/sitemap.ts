f92567b5dcb33615dfdc2c27281fb773b03ffeb2
Sebastian Sdorra <s.sdorra@gmail.com>
2024-05-14 08:20:21 +0200

    fix(website): double slash in sitemap urls

--- website/app/sitemap.ts
+++ website/app/sitemap.ts
@@ -10,17 +10,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     ...allQuickstarts.map((quickstart) => ({
-      url: `${FQDN}/${quickstart.href}`,
+      url: `${FQDN}${quickstart.href}`,
       lastModified: quickstart.lastModified,
       priority: 0.8,
     })),
     ...allDocs.map((docPage) => ({
-      url: `${FQDN}/${docPage.href}`,
+      url: `${FQDN}${docPage.href}`,
       lastModified: docPage.lastModified,
       priority: 0.7,
     })),
     ...allSamples.map((sample) => ({
-      url: `${FQDN}/${sample.href}`,
+      url: `${FQDN}${sample.href}`,
       lastModified: sample.lastModified,
       priority: 0.7,
     })),
