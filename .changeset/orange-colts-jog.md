29cbe9f7374fe7fbaf9b4546c8c782befe2a6d6b
Sebastian Sdorra <s.sdorra@gmail.com>
2024-05-28 09:13:29 +0200

    feat(next): #120 add turbopack support
    Use process parameters to differentiate between build and dev instead of using webpack
    
    Closes: #120

---
+++ .changeset/orange-colts-jog.md
@@ -0,0 +1,5 @@
+---
+"@content-collections/next": minor
+---
+
+Support for turbopack
--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -1,8 +1,5 @@
 import { configureLogging } from "@content-collections/integrations";
 import type { NextConfig } from "next";
-import type webpack from "webpack";
-
-let initialized = false;
 
 type Options = {
   configPath: string;
@@ -12,52 +9,29 @@ const defaultOptions: Options = {
   configPath: "content-collections.ts",
 };
 
-// use dynamic import if the next package is used in a commonjs environment
-const coreImport = import("@content-collections/core");
-
-class ContentCollectionWebpackPlugin {
-  constructor(readonly options: Options) {}
-
-  apply(compiler: webpack.Compiler) {
-    compiler.hooks.beforeCompile.tapPromise(
-      "contentCollectionWebpackPlugin",
-      async () => {
-        if (initialized) {
-          return;
-        }
-        initialized = true;
-
-        const { createBuilder } = await coreImport;
-
-        console.log("Starting content-collections", this.options.configPath);
-        const builder = await createBuilder(this.options.configPath);
-        configureLogging(builder);
-        await builder.build();
-
-        if (compiler.watchMode) {
-          console.log("start watching ...");
-          await builder.watch();
-        }
-      }
-    );
-  }
-}
-
 export function createContentCollectionPlugin(pluginOptions: Options) {
-  const plugin = new ContentCollectionWebpackPlugin(pluginOptions);
-  return (nextConfig: Partial<NextConfig> = {}): Partial<NextConfig> => {
-    return {
-      ...nextConfig,
-      webpack(config, options) {
-        config.plugins!.push(plugin);
+  return async (
+    nextConfig: Partial<NextConfig> = {}
+  ): Promise<Partial<NextConfig>> => {
+    const [command] = process.argv
+      .slice(2)
+      .filter((arg) => !arg.startsWith("-"));
+    if (command === "build" || command === "dev") {
+      const { createBuilder } = await import("@content-collections/core");
 
-        if (typeof nextConfig.webpack === "function") {
-          return nextConfig.webpack(config, options);
-        }
+      console.log("Starting content-collections", pluginOptions.configPath);
+      const builder = await createBuilder(pluginOptions.configPath);
+      configureLogging(builder);
 
-        return config;
-      },
-    };
+      await builder.build();
+
+      if (command === "dev") {
+        console.log("start watching ...");
+        await builder.watch();
+      }
+    }
+
+    return nextConfig;
   };
 }
 
