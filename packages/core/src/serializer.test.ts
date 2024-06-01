82f3e7155662622fa76983c75c13d8cdf8b3442f
Sebastian Sdorra <s.sdorra@gmail.com>
2024-06-01 21:47:50 +0200

    feat(core): #29 allow Date, Map, Set and BigInt as data types
    Closes: #29

--- packages/core/src/serializer.test.ts
+++ packages/core/src/serializer.test.ts
@@ -30,13 +30,46 @@ describe("serializer", () => {
     expect(result.success).toBe(true);
   });
 
-  it("should fail if object contains a date object", () => {
+  it("should allow date values", () => {
     const json = {
       a: new Date(),
     };
 
     const result = serializableSchema.safeParse(json);
-    expect(result.success).toBe(false);
+    expect(result.success).toBe(true);
+  });
+
+  it("should allow map values", () => {
+    const map = new Map<string,string>();
+    map.set("a", "b");
+
+    const json = {
+      a: map,
+    };
+
+    const result = serializableSchema.safeParse(json);
+    expect(result.success).toBe(true);
+  });
+
+  it("should allow set values", () => {
+    const set = new Set<string>();
+    set.add("a");
+
+    const json = {
+      a: set,
+    };
+
+    const result = serializableSchema.safeParse(json);
+    expect(result.success).toBe(true);
+  });
+
+  it("should allow bigint values", () => {
+    const json = {
+      a: BigInt(1),
+    };
+
+    const result = serializableSchema.safeParse(json);
+    expect(result.success).toBe(true);
   });
 
   it("should fail if object contains a function", () => {
@@ -48,17 +81,73 @@ describe("serializer", () => {
     expect(result.success).toBe(false);
   });
 
-  tmpdirTest("should serialize a valid object", async ({tmpdir}) => {
-    const object = {
-      firstName: "Tricia",
-      lastName: "McMillan",
-    };
+  describe("serialize", () => {
 
-    const result = serialize([object]);
-    const filePath = path.join(tmpdir, `test.${extension}`);
-    await fs.writeFile(tmpdir + `/test.${extension}`, result);
+    async function writeAndImport(tmpdir: string, object: unknown) {
+      const result = serialize([object]);
+      const filePath = path.join(tmpdir, `test.${extension}`);
+      await fs.writeFile(tmpdir + `/test.${extension}`, result);
 
-    const imported = await import(filePath + `?x=${Date.now()}`);
-    expect(imported.default).toEqual([object]);
+      const imported = await import(filePath + `?x=${Date.now()}`);
+      return imported.default;
+    }
+
+    tmpdirTest("should serialize a simple object", async ({tmpdir}) => {
+      const object = {
+        firstName: "Tricia",
+        lastName: "McMillan",
+      };
+
+      const imported = await writeAndImport(tmpdir, object);
+      expect(imported).toEqual([object]);
+    });
+
+    tmpdirTest("should serialize an object with a date", async ({tmpdir}) => {
+      const object = {
+        date: new Date("2021-01-01"),
+      };
+
+      const imported = await writeAndImport(tmpdir, object);
+      expect(imported).toEqual([object]);
+      expect(imported[0].date).toBeInstanceOf(Date);
+    });
+
+    tmpdirTest("should serialize an object with a map", async ({tmpdir}) => {
+      const map = new Map<string,string>();
+      map.set("a", "b");
+
+      const object = {
+        map,
+      };
+
+      const imported = await writeAndImport(tmpdir, object);
+      expect(imported).toEqual([object]);
+      expect(imported[0].map).toBeInstanceOf(Map);
+    });
+
+    tmpdirTest("should serialize an object with a set", async ({tmpdir}) => {
+      const set = new Set<string>();
+      set.add("a");
+
+      const object = {
+        set,
+      };
+
+      const imported = await writeAndImport(tmpdir, object);
+      expect(imported).toEqual([object]);
+      expect(imported[0].set).toBeInstanceOf(Set);
+    });
+
+    tmpdirTest("should serialize an object with a bigint", async ({tmpdir}) => {
+      const object = {
+        bigint: BigInt(1),
+      };
+
+      const imported = await writeAndImport(tmpdir, object);
+      expect(imported).toEqual([object]);
+      expect(imported[0].bigint).toBeTypeOf("bigint");
+    });
+
   });
+
 });
--- packages/core/src/serializer.ts
+++ packages/core/src/serializer.ts
@@ -2,31 +2,44 @@ import z from "zod";
 import serializeJs from "serialize-javascript";
 
 const literalSchema = z.union([
+  // json
   z.string(),
   z.number(),
   z.boolean(),
   z.null(),
+  // serializable-javascript
   z.undefined(),
+  z.date(),
+  z.map(z.unknown(), z.unknown()),
+  z.set(z.unknown()),
+  z.bigint(),
 ]);
 
 type Literal = z.infer<typeof literalSchema>;
 
-type Json = Literal | { [key: string]: Json } | Json[];
+type SchemaType = Literal | { [key: string]: SchemaType } | SchemaType[];
 
-const jsonSchema: z.ZodType<Json> = z.lazy(() =>
-  z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)])
+const schema: z.ZodType<SchemaType> = z.lazy(() =>
+  z.union([literalSchema, z.array(schema), z.record(schema)])
 );
 
 export type NotSerializableError =
-  "The return type of the transform function must be an valid JSONObject, the following type is not valid:";
+  `The return type of the transform function must be an object serializable object.
+See https://www.content-collections.dev/docs/transform#serializable for more information.
+
+The following type is not valid:`;
 
 export const extension = "js";
 
-export const serializableSchema = z.record(jsonSchema);
+export const serializableSchema = z.record(schema);
 
 export type Serializable = z.infer<typeof serializableSchema>;
 
 export function serialize(value: Array<unknown>): string {
-  const serializedValue = serializeJs(value, { space: 2, isJSON: true, unsafe: true });
+  const serializedValue = serializeJs(value, {
+    space: 2,
+    unsafe: true,
+    ignoreFunction: true,
+  });
   return `export default ${serializedValue};`;
 }
