5b2b2e62c78a8889d7a388268848cc3a1b0d0444
Sebastian Sdorra <s.sdorra@gmail.com>
2024-07-14 21:45:38 +0200

    test(core): #132 use tmpdirTest for watcher tests

--- packages/core/src/watcher.test.ts
+++ packages/core/src/watcher.test.ts
@@ -1,15 +1,14 @@
-import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
+import { describe, expect, vi, beforeEach } from "vitest";
 import fs from "node:fs/promises";
-import { existsSync } from "node:fs";
 import { createWatcher } from "./watcher";
 import { Modification } from "./types";
 import { Events, createEmitter } from "./events";
+import { tmpdirTest } from "./__tests__/tmpdir";
+import path from "node:path";
 
 const isEnabled = process.env.ENABLE_WATCHER_TESTS === "true";
 
 describe.runIf(isEnabled)("watcher", () => {
-  const directories: Array<string> = [];
-
   const events: Array<string> = [];
   let build = false;
 
@@ -26,9 +25,16 @@ describe.runIf(isEnabled)("watcher", () => {
     return;
   }
 
-  function findEvent(type: string, path: string, eventArray = events) {
+  function findEvent(
+    type: string,
+    baseDirectory: string,
+    pathName: string,
+    eventArray = events
+  ) {
     return eventArray.find(
-      (event) => event.startsWith(`${type}:`) && event.endsWith(path)
+      (event) =>
+        event.startsWith(`${type}:`) &&
+        event.endsWith(path.join(baseDirectory, pathName))
     );
   }
 
@@ -40,161 +46,140 @@ describe.runIf(isEnabled)("watcher", () => {
     build = false;
   });
 
-  afterEach(async () => {
-    for (const directory of directories.reverse()) {
-      if (existsSync(directory)) {
-        await fs.rm(directory, { recursive: true });
-      }
-    }
-    directories.length = 0;
-  });
+  const mkdir = async (baseDirectory: string, pathName: string) => {
+    const p = path.join(baseDirectory, pathName);
+    await fs.mkdir(p, { recursive: true });
 
-  const mkdir = async (path: string) => {
-    if (existsSync(path)) {
-      await fs.rm(path, { recursive: true });
-    }
-    await fs.mkdir(path);
-
-    directories.push(path);
+    return p;
   };
 
-  it("should not build", async () => {
-    await mkdir("tmp");
-
-    const watcher = await createWatcher(emitter, ["tmp"], syncFn, buildFn);
+  tmpdirTest("should not build", async ({ tmpdir }) => {
+    const watcher = await createWatcher(emitter, [tmpdir], syncFn, buildFn);
 
     await watcher.unsubscribe();
 
     expect(build).toBe(false);
   });
 
-  it("should not build for untracked file", async () => {
-    await mkdir("tmp");
-
-    const watcher = await createWatcher(emitter, ["tmp"], syncFn, buildFn);
+  tmpdirTest("should not build for untracked file", async ({ tmpdir }) => {
+    const watcher = await createWatcher(emitter, [tmpdir], syncFn, buildFn);
 
     // we create one file and wait for the event
     // because we receive often a event for the directory
-    await fs.writeFile("tmp/foo-ignore", "foo");
-    await vi.waitUntil(() => findEvent("create", "tmp/foo-ignore"));
+    await fs.writeFile(path.join(tmpdir, "foo-ignore"), "foo");
+    await vi.waitUntil(() => findEvent("create", tmpdir, "foo-ignore"));
 
     // we reset the build flag and create a second file
     build = false;
-    await fs.writeFile("tmp/bar-ignore", "foo");
-    await vi.waitUntil(() => findEvent("create", "tmp/bar-ignore"));
+    await fs.writeFile(path.join(tmpdir, "bar-ignore"), "foo");
+    await vi.waitUntil(() => findEvent("create", tmpdir, "bar-ignore"));
 
     await watcher.unsubscribe();
 
     expect(build).toBe(false);
   });
 
-  it("should emit create event", async () => {
-    await mkdir("tmp");
+  tmpdirTest("should emit create event", async ({ tmpdir }) => {
+    const watcher = await createWatcher(emitter, [tmpdir], syncFn, buildFn);
+    await fs.writeFile(path.join(tmpdir, "foo"), "foo");
 
-    const watcher = await createWatcher(emitter, ["tmp"], syncFn, buildFn);
-    await fs.writeFile("tmp/foo", "foo");
-
-    await vi.waitUntil(() => findEvent("create", "tmp/foo"));
+    await vi.waitUntil(() => findEvent("create", tmpdir, "foo"));
 
     await watcher.unsubscribe();
 
-    expect(findEvent("create", "tmp/foo")).toBeTruthy();
+    expect(findEvent("create", tmpdir, "foo")).toBeTruthy();
     expect(build).toBe(true);
   });
 
-  it("should emit update event", async () => {
-    await mkdir("tmp");
+  tmpdirTest("should emit update event", async ({ tmpdir }) => {
+    const watcher = await createWatcher(emitter, [tmpdir], syncFn, buildFn);
 
-    const watcher = await createWatcher(emitter, ["tmp"], syncFn, buildFn);
+    await fs.writeFile(path.join(tmpdir, "foo"), "foo", "utf-8");
+    await vi.waitUntil(() => findEvent("create", tmpdir, "foo"), 2000);
 
-    await fs.writeFile("tmp/foo", "foo", "utf-8");
-    await vi.waitUntil(() => findEvent("create", "tmp/foo"), 2000);
-
-    await fs.writeFile("tmp/foo", "bar", "utf-8");
-    await vi.waitUntil(() => findEvent("update", "tmp/foo"), 2000);
+    await fs.writeFile(path.join(tmpdir, "foo"), "bar", "utf-8");
+    await vi.waitUntil(() => findEvent("update", tmpdir, "foo"), 2000);
 
     await watcher.unsubscribe();
 
-    expect(findEvent("update", "tmp/foo")).toBeTruthy();
+    expect(findEvent("update", tmpdir, "foo")).toBeTruthy();
     expect(build).toBe(true);
   });
 
-  it("should emit delete event", async () => {
-    await mkdir("tmp");
-    await fs.writeFile("tmp/foo", "foo");
+  tmpdirTest("should emit delete event", async ({ tmpdir }) => {
+    await fs.writeFile(path.join(tmpdir, "foo"), "foo");
 
-    const watcher = await createWatcher(emitter, ["tmp"], syncFn, buildFn);
-    await fs.rm("tmp/foo");
+    const watcher = await createWatcher(emitter, [tmpdir], syncFn, buildFn);
+    await fs.rm(path.join(tmpdir, "foo"));
 
-    await vi.waitUntil(() => findEvent("delete", "tmp/foo"));
+    await vi.waitUntil(() => findEvent("delete", tmpdir, "foo"));
 
     await watcher.unsubscribe();
 
-    expect(findEvent("delete", "tmp/foo")).toBeTruthy();
+    expect(findEvent("delete", tmpdir, "foo")).toBeTruthy();
     expect(build).toBe(true);
   });
 
-  it("should emit events from multiple directories", async () => {
-    await mkdir("tmp-one");
-    await mkdir("tmp-two");
+  tmpdirTest(
+    "should emit events from multiple directories",
+    async ({ tmpdir }) => {
+      const one = await mkdir(tmpdir, "one");
+      const two = await mkdir(tmpdir, "two");
 
-    const watcher = await createWatcher(
-      emitter,
-      ["tmp-one", "tmp-two"],
-      syncFn,
-      buildFn
-    );
-    await fs.writeFile("tmp-one/foo", "foo");
-    await fs.writeFile("tmp-two/bar", "bar");
+      const watcher = await createWatcher(emitter, [one, two], syncFn, buildFn);
+      await fs.writeFile(path.join(one, "foo"), "foo");
+      await fs.writeFile(path.join(two, "bar"), "bar");
 
-    await vi.waitUntil(
-      () =>
-        findEvent("create", "tmp-one/foo") && findEvent("create", "tmp-two/bar")
-    );
+      await vi.waitUntil(
+        () => findEvent("create", one, "foo") && findEvent("create", two, "bar")
+      );
 
-    await watcher.unsubscribe();
+      await watcher.unsubscribe();
 
-    expect(findEvent("create", "tmp-one/foo")).toBeTruthy();
-    expect(findEvent("create", "tmp-two/bar")).toBeTruthy();
-    expect(build).toBe(true);
-  });
+      expect(findEvent("create", one, "foo")).toBeTruthy();
+      expect(findEvent("create", two, "bar")).toBeTruthy();
+      expect(build).toBe(true);
+    }
+  );
 
-  it("should emit events from nested directories", async () => {
-    await mkdir("tmp");
-    await mkdir("tmp/foo");
-    await mkdir("tmp/bar");
+  tmpdirTest(
+    "should emit events from nested directories",
+    async ({ tmpdir }) => {
+      const foo = await mkdir(tmpdir, "foo");
+      const bar = await mkdir(tmpdir, "bar");
 
-    const watcher = await createWatcher(emitter, ["tmp"], syncFn, buildFn);
-    await fs.writeFile("tmp/foo/baz", "baz");
-    await fs.writeFile("tmp/bar/qux", "qux");
+      const watcher = await createWatcher(emitter, [tmpdir], syncFn, buildFn);
+      await fs.writeFile(path.join(foo, "baz"), "baz");
+      await fs.writeFile(path.join(bar, "qux"), "qux");
 
-    await vi.waitUntil(
-      () =>
-        findEvent("create", "tmp/foo/baz") && findEvent("create", "tmp/bar/qux")
-    );
+      await vi.waitUntil(
+        () => findEvent("create", foo, "baz") && findEvent("create", bar, "qux")
+      );
 
-    await watcher.unsubscribe();
+      await watcher.unsubscribe();
 
-    expect(findEvent("create", "tmp/foo/baz")).toBeTruthy();
-    expect(findEvent("create", "tmp/bar/qux")).toBeTruthy();
-    expect(build).toBe(true);
-  });
+      expect(findEvent("create", foo, "baz")).toBeTruthy();
+      expect(findEvent("create", bar, "qux")).toBeTruthy();
+      expect(build).toBe(true);
+    }
+  );
 
-  it("should file change event to event emitter", async () => {
-    await mkdir("tmp");
+  tmpdirTest(
+    "should file change event to event emitter",
+    async ({ tmpdir }) => {
+      const localEvents: Array<string> = [];
+      emitter.on("watcher:file-changed", ({ modification, filePath }) =>
+        localEvents.push(`${modification}:${filePath}`)
+      );
 
-    const localEvents: Array<string> = [];
-    emitter.on("watcher:file-changed", ({ modification, filePath }) =>
-      localEvents.push(`${modification}:${filePath}`)
-    );
+      const watcher = await createWatcher(emitter, [tmpdir], syncFn, buildFn);
+      await fs.writeFile(path.join(tmpdir, "foo"), "foo");
 
-    const watcher = await createWatcher(emitter, ["tmp"], syncFn, buildFn);
-    await fs.writeFile("tmp/foo", "foo");
+      await vi.waitUntil(() => findEvent("create", tmpdir, "foo", localEvents));
 
-    await vi.waitUntil(() => findEvent("create", "tmp/foo", localEvents));
+      await watcher.unsubscribe();
 
-    await watcher.unsubscribe();
-
-    expect(findEvent("create", "tmp/foo", localEvents)).toBeTruthy();
-  });
+      expect(findEvent("create", tmpdir, "foo", localEvents)).toBeTruthy();
+    }
+  );
 });
