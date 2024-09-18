cba9cc1413da9b8ec772ec4058e2473273d08ccd
Sebastian Sdorra <s.sdorra@gmail.com>
2024-09-18 21:08:31 +0200

    test(solid-start): fix e2e tests for refactored sample

--- samples/solid/tests/collection.spec.ts
+++ samples/solid/tests/collection.spec.ts
@@ -4,11 +4,39 @@ test.beforeEach(async ({ page }) => {
   await page.goto("/");
 });
 
-test("should have content for character", async ({ page }) => {
+test("should have content for posts", async ({ page }) => {
   await expect(
-    page.getByRole("heading", { name: "Arthur Dent" }),
+    page.getByRole("heading", { name: "When a Parrot Became Our Tour Guide" }),
   ).toBeVisible();
   await expect(
-    page.getByRole("heading", { name: "Ford Prefect" }),
+    page.getByRole("heading", { name: "When a Monkey Hijacked Our Drone" }),
+  ).toBeVisible();
+  await expect(
+    page.getByRole("heading", {
+      name: "The Day a Kangaroo Borrowed My Camera",
+    }),
+  ).toBeVisible();
+});
+
+test("should navigate to post", async ({ page }) => {
+  await page.goto("/");
+
+  const heading = await page.getByRole("heading", {
+    name: "When a Monkey Hijacked Our Drone",
+  });
+  await heading.click();
+
+  await expect(page).toHaveURL(new RegExp("/posts/monkey$"));
+});
+
+test("should render post", async ({ page }) => {
+  await page.goto("/posts/parrot");
+
+  await expect(
+    page.getByRole("heading", { name: "When a Parrot Became Our Tour Guide" }),
+  ).toBeVisible();
+
+  await expect(
+    page.getByRole("heading", { name: "The Validators' Hurdles: Polly's Peculiar Picks" }),
   ).toBeVisible();
 });
