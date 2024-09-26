6b1c17aa8c78a9f5e00133cfa8af000aafb0f26f
Sebastian Sdorra <s.sdorra@gmail.com>
2024-09-26 12:55:42 +0200

    fix(mdx): #330 export cjs bundle
    Export commonjs bundle in addition to the esm bundle
    
    Closes: #329

--- packages/mdx/tsup.config.ts
+++ packages/mdx/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts", "src/react/server.tsx", "src/react/client.tsx"],
-  format: "esm",
+  format: ["esm", "cjs"],
   dts: true,
   external: ["react"],
   outDir: "dist",
