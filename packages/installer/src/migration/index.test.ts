f8bde04b4437799d551e9e4722d2a037a825f7f6
Sebastian Sdorra <s.sdorra@gmail.com>
2024-10-26 14:13:42 +0200

    feat(installer): #32 add support for remix

--- packages/installer/src/migration/index.test.ts
+++ packages/installer/src/migration/index.test.ts
@@ -10,6 +10,20 @@ describe("findMigrator", () => {
       },
     });
     expect(migrator).not.toBe(null);
+    expect(migrator.name).toBe("next");
+  });
+
+  it("should find remix migrator", () => {
+    const migrator = findMigrator({
+      name: "remix",
+      dependencies: {
+        "@remix-run/node": "^2.13.1",
+        "@remix-run/react": "^2.13.1",
+        "@remix-run/serve": "^2.13.1",
+      },
+    });
+    expect(migrator).not.toBe(null);
+    expect(migrator.name).toBe("remix");
   });
 
   it("should throw error if migrator could not be found", () => {
