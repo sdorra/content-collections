6fbdaa931cbf452938b831d8a8bdc8b071a38953
Sebastian Sdorra <s.sdorra@gmail.com>
2024-12-08 21:34:56 +0100

    sample(next): use different ports for samples

--- samples/next-mdx-static-import/playwright.config.ts
+++ samples/next-mdx-static-import/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:3003",
+    baseURL: "http://localhost:3004",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -40,8 +40,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm run dev --port=3003",
-    url: "http://localhost:3003",
+    command: "pnpm run dev --port=3004",
+    url: "http://localhost:3004",
     reuseExistingServer: !process.env.CI,
   },
 });
