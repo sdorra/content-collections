d3f2f776821bbc83a7bba8cad9112a77020b145c
thejackshelton <me@jackshelton.com>
2025-03-29 23:55:23 -0500

    feat: add initial component

---
+++ packages/mdx/src/qwik/mdx.test.tsx
@@ -0,0 +1,1 @@
+// TODO: tests w/ content collections + mdx-bundler for Qwik
---
+++ packages/mdx/src/qwik/mdx.tsx
@@ -0,0 +1,6 @@
+/* @jsxImportSource @builder.io/qwik */
+import { component$ } from "@builder.io/qwik";
+
+export const MDXContent = component$(() => {
+  return <div>MDXContent</div>;
+});
