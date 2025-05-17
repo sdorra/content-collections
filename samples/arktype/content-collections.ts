2f83b75cc7da74b2d45bc5e388790e36791e2221
Sebastian Sdorra <s.sdorra@gmail.com>
2025-05-17 20:50:06 +0200

    sample: #528 add sample for validation with arktype

---
+++ samples/arktype/content-collections.ts
@@ -0,0 +1,33 @@
+import { defineCollection, defineConfig } from "@content-collections/core";
+import { type } from "arktype"
+
+const characters = defineCollection({
+  name: "characters",
+  directory: "characters",
+  include: "*.yaml",
+  parser: "yaml",
+  schema: type({
+    id: "number",
+    name: "string",
+    status: "string",
+    species: "string",
+    "type": "string",
+    gender: "string",
+    origin: {
+      name: "string",
+      url: "string.url | string == 0"
+    },
+    location: {
+      name: "string",
+      url: "string.url | string == 0"
+    },
+    image: "string.url",
+    episode: "string.url[]",
+    url: "string.url",
+    created: "string",
+  }),
+});
+
+export default defineConfig({
+  collections: [characters],
+});
