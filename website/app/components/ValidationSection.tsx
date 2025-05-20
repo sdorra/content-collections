9fdb275b273b734b9fd6af1b3bed9071b6d58ae2
Sebastian Sdorra <s.sdorra@gmail.com>
2025-05-20 13:15:04 +0200

    chore(website): #528 update landing page for use of StandardSchema

--- website/app/components/ValidationSection.tsx
+++ website/app/components/ValidationSection.tsx
@@ -5,42 +5,60 @@ import { codeToJsx } from "@/lib/codeToJsx";
 import { CodeMotion } from "./CodeMotion";
 import { Content, Section, Title } from "./Section";
 
-const code = `const samples = defineCollection({
+const code = `/* cmp-line:imports */
+
+const samples = defineCollection({
   name: "samples",
   directory: "src/samples",
   include: "**/*.md",
-  schema: z.object({
-    /* cmp-line:codemotion */
-  }),
+  /* cmp-line:samples */
 });
 `;
 
-const simple = `const simple = {
-    name: z.string().min(1).max(120),
+const beer = {
+  import: `import { z } from "zod";`,
+  code: `defineCollection({
+  schema: z.object({
+    name: z.string(),
+    volume: z.number().positive(),
+    brewery: z.string(),
+    ingredients: z.array(z.string()),
     description: z.string().optional(),
-  }`;
+  })
+})`
+};
 
-const post = `const post = {
-    title: z.string().min(20).max(160),
-    summary: z.string().optional(),
-    draft: z.boolean(),
-    tags: z.array(z.string()),
-    publishedAt: z.string().regex(/\\d{4}-\\d{2}-\\d{2}/),
-  }`;
+const post = {
+  import: `import * as v from "valibot";`,
+  code: `defineCollection({
+  schema: v.object({
+    title: v.pipe(v.string(), v.maxLength(160)),
+    summary: v.optional(v.string()),
+    draft: v.boolean(),
+    tags: v.array(v.string()),
+    publishedAt: v.date(),
+  })
+})`
+};
 
-const author = `const author = {
-    id: z.number().positive(),
-    email: z.string().email(),
-    displayName: z.string().min(1).max(120),
-    birthday: z.string().regex(/\\d{4}-\\d{2}-\\d{2}/),
-  }`;
+const author = {
+  import: `import { type } from "arktype";`,
+  code: `defineCollection({
+  schema: type({
+    id: "number > 0",
+    email: "string.email",
+    displayName: "10 < string <= 120",
+    birthday: /\\d{4}-\\d{2}-\\d{2}/,
+  })
+})`
+};
 
-const sampleCode = [simple, post, author];
+const sampleSnippets = [beer, post, author];
 
 export async function ValidationSection() {
   const samples = await Promise.all(
-    sampleCode.map((sample) =>
-      codeToJsx(sample.trim(), {
+    sampleSnippets.map((sample) =>
+      codeToJsx(sample.code.trim(), {
         lang: "ts",
         type: "inline",
         log: false,
@@ -50,12 +68,23 @@ export async function ValidationSection() {
     ),
   );
 
+  const importsCode = await Promise.all(
+    sampleSnippets.map((sample) =>
+      codeToJsx(sample.import.trim(), {
+        lang: "ts",
+        type: "inline",
+        log: false,
+      }),
+    ),
+  );
+
   const api = await codeToJsx(code, {
     lang: "ts",
     log: false,
     transformers: [
       commentComponentTransformer({
-        codemotion: <CodeMotion>{samples}</CodeMotion>,
+        imports: <CodeMotion>{importsCode}</CodeMotion>,
+        samples: <CodeMotion>{samples}</CodeMotion>,
       }),
     ],
   });
@@ -63,17 +92,21 @@ export async function ValidationSection() {
   return (
     <Section>
       <Content className="grid grid-cols-1 items-center gap-10 md:grid-cols-2">
-        <Editor className="order-2 h-[23rem] md:order-none">{api}</Editor>
+        <Editor className="order-2 h-[25rem] md:order-none">{api}</Editor>
         <div className="flex flex-col gap-5">
           <Title>Powerful Validation</Title>
           <p>
             Content Collections takes care of validating your content based on
             your defined schema. Any document that doesn&apos;t match the schema
             will not be added to your collection. The schema is powered by{" "}
-            <ExternalLink href="https://zod.dev">Zod</ExternalLink>, which means
-            you can leverage all the robust validation features of{" "}
-            <ExternalLink href="https://zod.dev">Zod</ExternalLink> to ensure
-            the integrity of your content.
+            <ExternalLink href="https://standardschema.dev/">
+              StandardSchema
+            </ExternalLink>
+            , which means you can use any StandardSchema{" "}
+            <ExternalLink href="https://standardschema.dev/#what-schema-libraries-implement-the-spec">
+              compatible library
+            </ExternalLink>{" "}
+            to ensure the integrity of your content.
           </p>
           <p>
             During development, Content Collections will provide warnings for
