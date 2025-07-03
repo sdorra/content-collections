bec682f1a22755baaa60ac0f03bcf52eeb29dfdd
Sebastian Sdorra <s.sdorra@gmail.com>
2025-07-03 09:21:37 +0200

    fix(core): #602 synchronizing of nested files on windows (#603)
    Add missing test for syncing nested file changes.
    Fix Synchronizing of nested files on windows.
    Remove mocking of the path module causes issues with other tests.
    We no longer need it, as our CI runs on both Windows and Linux.
    
    Closes: #602

--- packages/core/src/synchronizer.test.ts
+++ packages/core/src/synchronizer.test.ts
@@ -1,4 +1,5 @@
-import { describe, expect, it, vitest } from "vitest";
+import path from "node:path";
+import { describe, expect, it } from "vitest";
 import { createSynchronizer } from "./synchronizer";
 import { CollectionFile, FileCollection, ResolvedCollection } from "./types";
 
@@ -122,6 +123,36 @@ describe("synchronizer", () => {
 
     expect(await synchronizer.changed("content/new.md")).toBe(true);
     expect(collection.files[0]?.data.content).toBe("changed");
+  });
+
+  it("should change file nested file", async () => {
+    const collection = {
+      directory: "content",
+      include: "*/test.json",
+      files: [
+        {
+          data: {
+            content: "",
+          },
+          path: path.join("a", "test.json"),
+        },
+      ],
+    };
+
+    const synchronizer = createSynchronizer(
+      createCollectionFileReader({
+        data: {
+          content: '{ "changed": true }',
+        },
+        path: path.join("a", "test.json"),
+      }),
+      [collection],
+    );
+
+    expect(
+      await synchronizer.changed(path.join("content", "a", "test.json")),
+    ).toBe(true);
+    expect(collection.files[0]?.data.content).toBe('{ "changed": true }');
   });
 
   it("should not add file, if path is not in collection directory", async () => {
@@ -317,73 +348,5 @@ describe("synchronizer", () => {
     expect(await synchronizer.changed("content/new.md")).toBe(false);
 
     expect(collection.files.length).toBe(0);
-  });
-
-  describe("posix", () => {
-    it("should add a new file", async () => {
-      vitest.mock("node:path", async (importOriginal) => {
-        const origin = await importOriginal<typeof import("node:path")>();
-        return {
-          default: {
-            ...origin,
-            sep: origin.posix.sep,
-            resolve: origin.posix.resolve,
-          },
-        };
-      });
-
-      const collection = {
-        directory: "content",
-        include: "**/*.md",
-        files: [],
-      };
-
-      const synchronizer = createSynchronizer(
-        createCollectionFileReader({
-          data: {
-            content: "changed",
-          },
-          path: "new.md",
-        }),
-        [collection],
-      );
-      expect(await synchronizer.changed("content/new.md")).toBe(true);
-
-      expect(collection.files.length).toBe(1);
-    });
-  });
-
-  describe("windows", () => {
-    it("should add a new file", async () => {
-      vitest.mock("node:path", async (importOriginal) => {
-        const origin = await importOriginal<typeof import("node:path")>();
-        return {
-          default: {
-            ...origin,
-            sep: origin.win32.sep,
-            resolve: origin.win32.resolve,
-          },
-        };
-      });
-
-      const collection = {
-        directory: "content",
-        include: "**/*.md",
-        files: [],
-      };
-
-      const synchronizer = createSynchronizer(
-        createCollectionFileReader({
-          data: {
-            content: "changed",
-          },
-          path: "new.md",
-        }),
-        [collection],
-      );
-      expect(await synchronizer.changed("content\\new.md")).toBe(true);
-
-      expect(collection.files.length).toBe(1);
-    });
   });
 });
