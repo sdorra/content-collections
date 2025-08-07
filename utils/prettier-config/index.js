5ae431d170867c2b5cc14523495b6ffcde3ca6e1
Sebastian Sdorra <s.sdorra@gmail.com>
2025-08-07 22:04:15 +0200

    chore: simplify prettier setup to fix vscode formatting

--- utils/prettier-config/index.js
+++
@@ -1,6 +0,0 @@
-import * as organizeImports from "prettier-plugin-organize-imports";
-import * as tailwindcss from "prettier-plugin-tailwindcss";
-
-export default {
-  plugins: [organizeImports, tailwindcss],
-};
