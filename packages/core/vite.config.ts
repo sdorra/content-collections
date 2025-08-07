6464b4963089470fb3a1ac6bdb524bdc618c45ae
Sebastian Sdorra <s.sdorra@gmail.com>
2025-08-07 20:21:58 +0200

    build(core): increase test timeout on windows ci

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from "vitest/config";
 
 const excludes = ["**/__tests__/**", "**/types.ts", "**/*.test.ts", "**/*.test-d.ts"];
 
+function isWindowsCI() {
+  return process.platform === "win32" && process.env.GITHUB_ACTIONS === "true";
+}
+
 export default defineConfig({
   test: {
     coverage: {
       include: ["src/**/*.ts"],
       exclude: excludes,
     },
+    testTimeout: isWindowsCI() ? 10_000 : 5_000,
     poolMatchGlobs: [["**/watcher.test.ts", "forks"]],
   },
 });
