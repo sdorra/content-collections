aa5196fe47ec45cf9fc93625ea24f42d1a1904d7
Sebastian Sdorra <s.sdorra@gmail.com>
2025-08-15 12:56:35 +0200

    wip: first watcher tests

--- packages/core/src/index.test.ts
+++ packages/core/src/index.test.ts
@@ -1,8 +1,8 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { z } from "zod";
 import { workspaceTest } from "./__tests__/workspace";
-import { defineCollection, defineConfig } from "./config";
 import { createBuilder } from "./builder";
+import { defineCollection, defineConfig } from "./config";
 import { defineParser } from "./parser";
 
 describe("workspace tests", () => {
@@ -881,6 +881,127 @@ describe("workspace tests", () => {
       const { collection } = await workspace.build();
       const allMovies = await collection("movies");
       expect(allMovies).toHaveLength(1);
+      expect(allMovies.map((m) => m.name)).toEqual(["Fight Club"]);
+    },
+  );
+
+  workspaceTest(
+    "should add new files to workspace",
+    async ({ workspaceBuilder }) => {
+      const movies = defineCollection({
+        name: "movies",
+        directory: "sources/movies",
+        include: "*.json",
+        parser: "json",
+        schema: z.object({
+          name: z.string(),
+          year: z.number(),
+        }),
+      });
+
+      const config = defineConfig({
+        collections: [movies],
+      });
+
+      const workspace = workspaceBuilder(config);
+
+      workspace.file(
+        "sources/movies/fight-club.json",
+        `{
+        "name": "Fight Club",
+        "year": 1999
+      }`,
+      );
+
+      workspace.file(
+        "sources/movies/inception.json",
+        `{
+        "name": "Inception",
+        "year": 2010
+      }`,
+      );
+
+      let { collection } = await workspace.build();
+
+      let allMovies = await collection("movies");
+      expect(allMovies).toHaveLength(2);
+      expect(allMovies.map((m) => m.name)).toEqual(["Fight Club", "Inception"]);
+
+      await workspace.watch();
+
+      await workspace.path("sources/movies/interstellar.json").write(
+        `{
+        "name": "Interstellar",
+        "year": 2014
+      }`,
+      );
+
+      allMovies = await vi.waitFor(async () => {
+        const col = await collection("movies");
+        expect(col).toHaveLength(3);
+        return col;
+      });
+
+      expect(allMovies.map((m) => m.name)).toEqual([
+        "Fight Club",
+        "Inception",
+        "Interstellar",
+      ]);
+    },
+  );
+
+  workspaceTest(
+    "should remove file from collection",
+    async ({ workspaceBuilder }) => {
+      const movies = defineCollection({
+        name: "movies",
+        directory: "sources/movies",
+        include: "*.json",
+        parser: "json",
+        schema: z.object({
+          name: z.string(),
+          year: z.number(),
+        }),
+      });
+
+      const config = defineConfig({
+        collections: [movies],
+      });
+
+      const workspace = workspaceBuilder(config);
+
+      workspace.file(
+        "sources/movies/fight-club.json",
+        `{
+        "name": "Fight Club",
+        "year": 1999
+      }`,
+      );
+
+      workspace.file(
+        "sources/movies/inception.json",
+        `{
+        "name": "Inception",
+        "year": 2010
+      }`,
+      );
+
+      let { collection } = await workspace.build();
+
+      let allMovies = await collection("movies");
+      expect(allMovies).toHaveLength(2);
+      expect(allMovies.map((m) => m.name)).toEqual(["Fight Club", "Inception"]);
+
+      await workspace.watch();
+
+      await workspace.path("sources/movies/inception.json").unlink();
+
+      allMovies = await vi.waitFor(async () => {
+        const col = await collection("movies");
+        expect(col).toHaveLength(1);
+        return col;
+      });
+
       expect(allMovies.map((m) => m.name)).toEqual(["Fight Club"]);
     },
   );
