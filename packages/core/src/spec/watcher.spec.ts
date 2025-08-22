f69349b4d215caefed9e69a3cf592ef9deca5613
Sebastian Sdorra <s.sdorra@gmail.com>
2025-08-22 22:05:15 +0200

    wip: add missing file update test

--- packages/core/src/spec/watcher.spec.ts
+++ packages/core/src/spec/watcher.spec.ts
@@ -5,7 +5,7 @@ import { workspaceTest } from "./workspace";
 
 describe("watcher", () => {
   workspaceTest(
-    "should add new files to workspace",
+    "should add new file to collection",
     async ({ workspaceBuilder }) => {
       const movies = defineCollection({
         name: "movies",
@@ -59,13 +59,77 @@ describe("watcher", () => {
         const col = await collection("movies");
         expect(col).toHaveLength(3);
         return col;
-      }, 2000);
+      }, 3000);
 
       expect(allMovies.map((m) => m.name)).toEqual([
         "Fight Club",
         "Inception",
         "Interstellar",
       ]);
+    },
+  );
+
+  workspaceTest(
+    "should update existing file in collection",
+    async ({ workspaceBuilder }) => {
+      const movies = defineCollection({
+        name: "movies",
+        directory: "sources/movies",
+        include: "*.json",
+        parser: "json",
+        schema: z.object({
+          name: z.string(),
+          year: z.number(),
+        }),
+      });
+
+      const config = defineConfig({
+        collections: [movies],
+      });
+
+      const workspace = workspaceBuilder(config);
+
+      workspace.file(
+        "sources/movies/fight-club.json",
+        `{
+        "name": "Fight Clubbb",
+        "year": 1999
+      }`,
+      );
+
+      workspace.file(
+        "sources/movies/inception.json",
+        `{
+        "name": "Inception",
+        "year": 2010
+      }`,
+      );
+
+      let { collection } = await workspace.build();
+
+      let allMovies = await collection("movies");
+      expect(allMovies).toHaveLength(2);
+      expect(allMovies.map((m) => m.name)).toEqual([
+        "Fight Clubbb",
+        "Inception",
+      ]);
+
+      await workspace.watch();
+
+      await workspace.path("sources/movies/fight-club.json").write(
+        `{
+        "name": "Fight Club",
+        "year": 1999
+      }`,
+      );
+
+      allMovies = await vi.waitFor(async () => {
+        const col = await collection("movies");
+        expect(col.map((m) => m.name)).toEqual(["Fight Club", "Inception"]);
+        return col;
+      }, 5000);
+
+      expect(allMovies.map((m) => m.name)).toEqual(["Fight Club", "Inception"]);
     },
   );
 
@@ -119,7 +183,7 @@ describe("watcher", () => {
         const col = await collection("movies");
         expect(col).toHaveLength(1);
         return col;
-      }, 2000);
+      }, 3000);
 
       expect(allMovies.map((m) => m.name)).toEqual(["Fight Club"]);
     },
