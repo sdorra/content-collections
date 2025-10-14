f52388fc2a2f6f4701ec90364f71f849f0c7d875
Joseph Chamochumbi <joseph.chamochumbi@vercel.com>
2025-10-14 13:04:32 +0200

    refactor: isDev detection with NODE_ENV

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -1,9 +1,5 @@
 import { configureLogging } from "@content-collections/integrations";
 import type { NextConfig } from "next";
-import {
-  PHASE_DEVELOPMENT_SERVER,
-  PHASE_PRODUCTION_BUILD,
-} from "next/constants.js";
 
 type Options = {
   configPath: string;
@@ -24,57 +20,60 @@ type NextConfigInput =
     ) => Partial<NextConfig> | Promise<Partial<NextConfig>>);
 
 export function createContentCollectionPlugin(pluginOptions: Options) {
-  return function withContentCollectionsPlugin(
+  const [command] = process.argv.slice(2).filter((arg) => !arg.startsWith("-"));
+
+  // typegen loads next.config.js
+  const isTypegen = command === "typegen";
+
+  // the build step loads next.config.js
+  const isBuild = command === "build";
+
+  // starting with v16 next dev doesn't load next.config.js
+  // next dev - calls next-start in a forked process
+  // next-start loads next.config.js
+  // process.argv are not visible by next-start
+  const isDev =
+    // to make this compatible with previous versions
+    // check if command is NOT set (next-start) and we are in development mode
+    typeof command === "undefined" && process.env.NODE_ENV === "development";
+
+  return async function withContentCollectionsPlugin(
     nextConfig: NextConfigInput = {},
-  ) {
-    return async function wrappedNextConfig(
-      phase: string,
-      defaults: { defaultConfig: NextConfig },
-    ): Promise<Partial<NextConfig>> {
-      const isBuild = phase === PHASE_PRODUCTION_BUILD;
-      const isDev =
-        phase === PHASE_DEVELOPMENT_SERVER ||
-        process.env.NODE_ENV === "development";
+  ): Promise<Partial<NextConfig>> {
+    if (isBuild || isDev || isTypegen) {
+      // In dev, avoid re-running in re-spawned child whose parent is PID 1
 
-      if (isBuild || isDev) {
-        // In dev, avoid re-running in re-spawned child whose parent is PID 1
+      // If the parent process is 1, we assume that the parent process was killed
+      // and we are running in a new process, so we skip the initialization
+      // https://github.com/sdorra/content-collections/issues/499
 
-        // If the parent process is 1, we assume that the parent process was killed
-        // and we are running in a new process, so we skip the initialization
-        // https://github.com/sdorra/content-collections/issues/499
+      // we do the check only in dev mode, because in environment like docker
+      // it is common that the build is directly started by the init process (pid 1).
+      // https://github.com/sdorra/content-collections/issues/511
+      const shouldInitialize = !(isDev && process.ppid === 1);
 
-        // we do the check only in dev mode, because in environment like docker
-        // it is common that the build is directly started by the init process (pid 1).
-        // https://github.com/sdorra/content-collections/issues/511
-        const shouldInitialize = !(isDev && process.ppid === 1);
+      if (shouldInitialize) {
+        const key = pluginOptions.configPath;
+        if (!initializedState[key]) {
+          initializedState[key] = true;
 
-        if (shouldInitialize) {
-          const key = pluginOptions.configPath;
-          if (!initializedState[key]) {
-            initializedState[key] = true;
+          const { createBuilder } = await import("@content-collections/core");
+          console.log("Starting content-collections", key);
 
-            const { createBuilder } = await import("@content-collections/core");
-            console.log("Starting content-collections", key);
+          const builder = await createBuilder(key);
+          configureLogging(builder);
 
-            const builder = await createBuilder(key);
-            configureLogging(builder);
+          await builder.build();
 
-            await builder.build();
-
-            if (isDev) {
-              console.log("start watching ...");
-              await builder.watch();
-            }
+          if (isDev) {
+            console.log("start watching ...");
+            await builder.watch();
           }
         }
       }
+    }
 
-      if (typeof nextConfig === "function") {
-        return await nextConfig(phase, defaults);
-      }
-
-      return nextConfig;
-    };
+    return nextConfig;
   };
 }
 
