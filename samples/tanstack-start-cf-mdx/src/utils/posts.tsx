6836ad4aec5b9a0fdc4c968d49171a4b7b7b7db2
Sebastian Sdorra <s.sdorra@gmail.com>
2026-04-16 15:31:35 +0200

    sample: use renderServerComponent to render mdx

--- samples/tanstack-start-cf-mdx/src/utils/posts.tsx
+++ samples/tanstack-start-cf-mdx/src/utils/posts.tsx
@@ -1,33 +1,26 @@
 import { notFound } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/react-start";
-import {
-  createFromReadableStream,
-  renderToReadableStream,
-} from "@tanstack/react-start/rsc";
+import { renderServerComponent } from "@tanstack/react-start/rsc";
 import { allPosts } from "content-collections";
 
-const getPostServerFn = createServerFn({ method: "GET" })
+const getPostBySlugServerFn = createServerFn({ method: "GET" })
   .inputValidator((slug: string) => slug)
   .handler(async ({ data: slug }) => {
     const post = allPosts.find((post) => post.slug === slug);
     if (!post) {
       throw notFound();
     }
+
     const MDXContent = post.mdx;
 
     return {
       ...post,
-      mdx: renderToReadableStream(<MDXContent />),
-    };
+      mdx: await renderServerComponent(<MDXContent />)
+    }
   });
 
-export async function getPostBySlug(slug: string) {
-  const { mdx, ...post } = await getPostServerFn({ data: slug });
-  return {
-    ...post,
-    mdx: await createFromReadableStream(mdx),
-  };
-}
+export const getPostBySlug = async (slug: string) =>
+  getPostBySlugServerFn({ data: slug });
 
 export const getAllPosts = createServerFn({ method: "GET" }).handler(
   async () => {
